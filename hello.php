f492ea22bc3c45a10fea52acef3f58b00e244079
Sangam Puri <sangampuree@gmail.com>
2025-11-24 19:36:11 +0545

    first php program

---
+++ hello.php
@@ -0,0 +1,15 @@
+<?php
+    // echo "Hello php "
+    $greeting = "Namaste"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hello.php</title>
+</head>
+<body>
+   <p> <?php echo $greeting ?>,user!</p>
+</body>
+</html>
